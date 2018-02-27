23e5005ad591170e3bd3f8e8507e35957ecf989f
Elias A Martinez <MartyFromCary@gmail.com>
2018-02-27 08:48:20 -0500

    Final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,126 @@
-const inquirer = require("inquirer");
+const prompt = require("inquirer").prompt;
 const conn = require("./connect");
 
 conn.connect();
+
+function formatPrice(price) {
+  let P = Math.floor(parseFloat(price) * 100).toString();
+  let decimalPos = P.length - 2;
+
+  return P.substr(0, decimalPos) + "." + P.substr(decimalPos);
+}
+function askGoOn() {
+  prompt([
+    {
+      name: "go_on",
+      type: "confirm",
+      message: "Would you like to continue"
+    }
+  ]).then(({ go_on }) => {
+    if (go_on) {
+      askDepartment();
+    } else {
+      conn.end();
+    }
+  });
+}
+
+function purchaseItem(item_id, product_name, price, quantity, stock_quantity) {
+  console.log(
+    `${item_id}, ${product_name}, ${price}, ${quantity}, ${stock_quantity}`
+  );
+  conn.query(
+    "UPDATE products SET ? WHERE ?",
+    [{ stock_quantity: stock_quantity - quantity }, { item_id }],
+    (err, res) => {
+      if (err) throw err;
+      console.log(
+        `You have purchased ${quantity} ${product_name}(s) @ $${formatPrice(
+          price
+        )}`
+      );
+      console.log(`You will be charged: $${formatPrice(quantity * price)}`);
+      askGoOn();
+    }
+  );
+}
+
+function askHowMany(item_id, product_name, price) {
+  conn.query(
+    "SELECT stock_quantity FROM products WHERE ?",
+    [{ item_id }],
+    (err, res) => {
+      if (err) throw err;
+      const { stock_quantity } = res[0];
+      console.log(stock_quantity);
+      prompt([
+        {
+          name: "quantity",
+          type: "input",
+          message: `How many ${product_name}(s) would "you like to order?\n${stock_quantity} are available.`
+        }
+      ]).then(({ quantity }) => {
+        console.log(quantity);
+        if (quantity <= stock_quantity) {
+          purchaseItem(item_id, product_name, price, quantity, stock_quantity);
+        } else {
+          console.log("Sorry, Insufficient quantity avaialable!");
+          askGoOn();
+        }
+      });
+    }
+  );
+}
+
+function askItem(department_name) {
+  conn.query(
+    "SELECT item_id,product_name,price FROM products WHERE ? AND stock_quantity > 0",
+    [{ department_name }],
+    (err, res) => {
+      if (err) throw err;
+      prompt([
+        {
+          name: "item_name",
+          type: "list",
+          choices: res.map(
+            tuple =>
+              tuple.item_id +
+              ",  " +
+              tuple.product_name +
+              ",  $" +
+              formatPrice(tuple.price)
+          ),
+          message: `Witch item from the ${department_name} Department would you like to order?`
+        }
+      ]).then(({ item_name }) => {
+        let tmpArr = item_name.split(",").map(S => S.trim());
+        askHowMany(
+          tmpArr.shift(), // item_id
+          tmpArr.shift(), // product_name
+          parseFloat(tmpArr.shift().substr(1)) //price
+        );
+      });
+    }
+  );
+}
+
+function askDepartment() {
+  conn.query(
+    "SELECT DISTINCT department_name FROM products WHERE stock_quantity > 0 ORDER BY department_name ASC",
+    (err, res) => {
+      if (err) throw err;
+      prompt([
+        {
+          name: "department_name",
+          type: "list",
+          choices: res.map(tuple => tuple.department_name),
+          message: "From which department would you like to order?"
+        }
+      ]).then(({ department_name }) => {
+        askItem(department_name);
+      });
+    }
+  );
+}
+
+askDepartment();
--- products.sql
+++ products.sql
@@ -1,3 +1,4 @@
+USE bamazon;
 DROP TABLE if exists products;
 
 CREATE TABLE products (
@@ -7,18 +8,25 @@ CREATE TABLE products (
     price FLOAT NOT NULL,
     stock_quantity INTEGER NOT NULL,
     PRIMARY KEY (item_id)
+    
 );
 
 INSERT INTO products
 (item_id,product_name,department_name,price,stock_quantity)
 VALUES
-(4711,  "Cargo Pants",          "Mens Wear",      20.99,    40),
-(4712,  "Skirt",                "Womans Wear",    59.99,    20),
-(4713,  "Onesy",                "Babies Wear",    19.95,    30),
-(4714,  "Dinette Set",          "Furniture",     249.99,    10),
-(4715,  "Pool Table",           "Furniture",     500.00,    15),
-(4716,  "Entertainment Center", "Furniture",    1500.00,    20),
-(4717,  "Tank Top",             "Mens Wear",      12.98,    20),
-(4718,  "Ladies Shoes",         "Shoes",          15,95,    12);
+(4711,  "Cargo Pants",          "Mens Wear",      20.99,    4),
+(4712,  "Skirt",                "Womans Wear",    59.99,    8),
+(4713,  "Onesy",                "Babies Wear",    19.95,    7),
+(4714,  "Dinette Set",          "Furniture",     249.99,    2),
+(4715,  "Pool Table",           "Furniture",     500.00,    6),
+(4716,  "Entertainment Center", "Furniture",    1500.00,    2),
+(4717,  "Tank Top",             "Mens Wear",      12.98,    8),
+(4718,  "Ladies Shoes",         "Shoes",          15.95,    5),
+(4719,  "Mens Sandals",         "Shoes",          12.00,    4),
+(4720,  "Barbie Doll",          "Toys",           15.00,    2),
+(4721,  "Camping Tent",         "Outdoor",       125.98,    9),
+(4722,  "Charcoal Grill",       "Outdoor",       119.98,    3),
+(4723,  "Baby Jacket",          "Babies Wear",    12.98,    2),
+(4724,  "Spider Man Action Figure","Toys",        11.50,    6);
 
 select * from products;
